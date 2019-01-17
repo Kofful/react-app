acc277bfecad90f881ccf6bb6f1225b607900c94
Kofful <44171932+Kofful@users.noreply.github.com>
2019-01-17 20:55:53 +0200

    Add files via upload

--- src/components/Calendar/Calendar.js
+++ src/components/Calendar/Calendar.js
@@ -1,30 +1,70 @@
 import React, { Component } from 'react';
 import "./Calendar.sass";
+const moment = require('moment');
 
 class Calendar extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            weekstart: "",
+            weekend: "",
+            days: [],
+        }
+        this.nextWeek = this.nextWeek.bind(this);
+    }
+    renderDate() {
+        this.state.weekstart = moment().startOf('week');
+        this.state.weekend = moment().endOf('week'); 
+        return (
+            <p className="position">{this.state.weekstart.format("ll")} - {this.state.weekend.format("ll")}</p>
+        );
+    }
+
+    selectDay(e) {
+    }
+
+    renderNumbers() {
+        const days = []
+        for(let i = moment(this.state.weekstart).format("DD"); i <= moment(this.state.weekend).format("DD"); i++)
+        {
+            this.state.days.push(i);
+            days.push(<div>
+                <p className="number">{i}</p>
+                </div>)
+        }
+        return (
+            <div className="numbers">
+            {days}
+            </div>
+        );
+    }
+
+    nextWeek() {
+        this.setState({
+            weekstart: moment(this.state.weekstart).add(7, "day"),
+            weekend: moment(this.state.weekend).add(7, "day")
+        })
     }
 
     render() {
         return (
             <>
+                <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet" />
                 <div className="header">
-                <p className="prev">PREV</p>
-                <p className="position">JUNE 18-24</p>
-                <p className="next">NEXT</p>
+                    <p className="prev">PREV</p>
+                    {this.renderDate()}
+                    <p className="next" onClick={this.nextWeek}>NEXT</p>
                 </div>
                 <div className="days">
-                <p className="day-of-week">S</p>
-                <p className="day-of-week">M</p>
-                <p className="day-of-week">T</p>
-                <p className="day-of-week">W</p>
-                <p className="day-of-week">T</p>
-                <p className="day-of-week">F</p>
-                <p className="day-of-week">S</p>
+                    <p>S</p>
+                    <p>M</p>
+                    <p>T</p>
+                    <p>W</p>
+                    <p>T</p>
+                    <p>F</p>
+                    <p>S</p>
                 </div>
-                <div>
-                </div>
+                {this.renderNumbers()}
             </>
         );
     }
